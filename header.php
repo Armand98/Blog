5a92f9ee4db2991949f9db6901838310d868d396
Armand98 <pajorarmand@gmail.com>
2019-11-13 18:51:28 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -3,6 +3,9 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" conent="Wiersze, cytaty i zagadki. Blog poświęcony twórczości poetyckiej.">
+	<meta name="keywords" content="blog, wiersze, cytaty, zagadki, refleksje, poezja, przemyślenia, pasja, twórczość">
+	<meta name="robots" content="index, follow">
 	<title>Armand Pajor</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link href="css/style.css" rel="stylesheet">
