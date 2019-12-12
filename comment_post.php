b6d61595ab08500231afc84163106a62af87fcaa
Armand <pajorarmand@gmail.com>
2019-12-12 13:50:59 +0100

    Add files via upload

--- comment_post.php
+++ comment_post.php
@@ -13,14 +13,16 @@
 		<?php 
 			$pid = $_GET['pid'];
 			$type = $_GET['type'];
-			$table = "post";
-			if ($type == 2)
-			{
+			if($type == 1) {
+				$table = "post";
+			} else if ($type == 2) {
 				$table = "quote";
-			}
-			else if ($type == 3)
-			{
+			} else if ($type == 3) {
 				$table = "puzzle";
+			} else {
+				exit();
+				echo '<script>alert("Nie kombinuj ;)")</script>';
+				echo '<script>window.location.replace("http://localhost/blog/index.php")</script>';
 			}
 			displayContent("SELECT * FROM $table WHERE id=$pid", $type); 
 		?>
